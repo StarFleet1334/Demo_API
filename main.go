d067af74529e40f9860cedc5ed8cd559f1ebd0e4
IliaLataria01 <latariailia6@gmail.com>
2024-03-23 23:23:38 +0400

    First Commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "net/http"
+
+func main() {
+	mux := http.NewServeMux()
+	mux.Handle("/", &homeHandler{})
+
+	http.ListenAndServe("localhost:8080", mux)
+}
+
+type homeHandler struct{}
+
+func (h *homeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("This is my home page"))
+}
